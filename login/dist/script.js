907b522206403108368c6d3ee470513cc2ba61d6
Varun <vrnvav97@rediffmail.com>
2019-06-26 00:11:55 +0530

    under process

--- login/dist/script.js
+++ login/dist/script.js
@@ -31,5 +31,10 @@ $(function() {
 	});
 });
 
-var password = document.querySelector ("form-styling");
+var pass = document.querySelector (".password");
 console.log(password);
+if (pass.length < 6 || pass.length>12)
+{
+  console.log("Executed this");
+alert("Password criteria doesn't matches");
+}
